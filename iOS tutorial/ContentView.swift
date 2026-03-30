46bd4f130f56055f1d8b46203c4fbff009c5b03a
AyomideCharles <dettyboy@icloud.com>
2026-03-30 08:50:57 +0100

    Initial Commit

---
+++ iOS tutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS tutorial
+//
+//  Created by Charles on 3/30/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS tutorial/iOS_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_tutorialApp.swift
+//  iOS tutorial
+//
+//  Created by Charles on 3/30/26.
+//
+
+import SwiftUI
+
+@main
+struct iOS_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
