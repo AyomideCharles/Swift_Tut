1b27ef0c9ef7d0dfca471953ef2897ef5c5c581d
AyomideCharles <dettyboy@icloud.com>
2026-04-05 23:54:41 +0100

    pushing progress

---
+++ iOS tutorial/ListAndTextView.swift
@@ -0,0 +1,18 @@
+//
+//  ListAndTextView.swift
+//  iOS tutorial
+//
+//  Created by Charles on 4/3/26.
+//
+
+import SwiftUI
+
+struct ListAndTextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListAndTextView()
+}
---
+++ iOS tutorial/Profile.swift
@@ -0,0 +1,18 @@
+//
+//  Profile.swift
+//  iOS tutorial
+//
+//  Created by Charles on 4/5/26.
+//
+
+import SwiftUI
+
+struct Profile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Profile()
+}
